25ab75e65e2bd467bc10b3e2e75cfccae5e3093c
Waverley Leung <waverley.nyc@gmail.com>
2020-06-07 23:20:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+//function which accepts two parameters an array of musicians and of instruments.
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var factsArray = []
+  let i = 0
+  while (factsArray.length < facts.length) {
+    factsArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return factsArray;
+}
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return array
+}
